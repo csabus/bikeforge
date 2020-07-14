c474976cc44642876bbe089cc243153f58fb3e2b
foldesi.csaba@idevelopment.hu <csabuskukac>
2020-07-14 15:39:31 +0200

    Base project

---
+++ src/main/java/hu/idevelopment/bikeforge/BikeforgeApplication.java
@@ -0,0 +1,13 @@
+package hu.idevelopment.bikeforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeforgeApplication.class, args);
+	}
+
+}
